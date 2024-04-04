2d37dd0c8a3292cba7c568b30d913c71bc01d2dd
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-04-04 14:06:32 +0200

    Added text module

---
+++ R/Text.R
@@ -0,0 +1,21 @@
+Text <- R6::R6Class(
+  classname = "Text",
+  inherit = Module,
+  public = list(
+    initialize = function(appId, mdFile) {
+      super$initialize(appId)
+      private$.mdText <- readLines(mdFile)
+    },
+
+    server = function() {},
+    ui = function() {
+      shiny::markdown(private$.mdText)
+    }
+  ),
+  private = list(
+    .mdText = ""
+  ),
+  active = list(
+    mdText = function() return(private$.mdText)
+  )
+)
