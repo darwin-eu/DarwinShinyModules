09ad2b0ee08ffd80ced95a86adb2a0ad0eaab98f
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-04-04 14:06:09 +0200

    Added DT bindings when for selecting data

--- R/Table.R
+++ R/Table.R
@@ -38,6 +38,7 @@ Table <- R6::R6Class(
     server = function(input, output, session) {
       private$renderTable(output)
       private$downloader(output)
+      private$setReactiveValues(input)
     }
   ),
 
@@ -45,8 +46,62 @@ Table <- R6::R6Class(
   private = list(
     ## Fields ----
     .data = NULL,
+    .reactiveValues = shiny::reactiveValues(
+      cell_clicked = NULL,
+      cells_selected = NULL,
+      cell_info = NULL,
+      rows_current = NULL,
+      rows_all = NULL,
+      rows_selected = NULL,
+      row_last_clicked = NULL,
+      columns_selected = NULL,
+      search = NULL,
+      search_columns = NULL,
+      state = NULL
+    ),
 
     ## Methods ----
+    setReactiveValues = function(input) {
+      cellClicked <- sprintf("%s_cell_clicked", private$id("table"))
+      cellsSelected <- sprintf("%s_cells_selected", private$id("table"))
+      cellInfo <- sprintf("%s_cell_info", private$id("table"))
+      rowsCurrent <- sprintf("%s_rows_current", private$id("table"))
+      rowsAll <- sprintf("%s_rows_all", private$id("table"))
+      rowsSelected <- sprintf("%s_rows_selected", private$id("table"))
+      rowLastClicked <- sprintf("%s_row_last_clicked", private$id("table"))
+      columnsSelected <- sprintf("%s_columns_selected", private$id("table"))
+      search <- sprintf("%s_search", private$id("table"))
+      searchColumns <- sprintf("%s_search_columns", private$id("table"))
+      state <- sprintf("%s_state", private$id("table"))
+
+      observeEvent(
+        c(
+          input[[cellClicked]],
+          input[[cellsSelected]],
+          input[[cellInfo]],
+          input[[rowsCurrent]],
+          input[[rowsAll]],
+          input[[rowsSelected]],
+          input[[rowLastClicked]],
+          input[[columnsSelected]],
+          input[[search]],
+          input[[searchColumns]],
+          input[[state]]
+        ), {
+        private$.reactiveValues$cell_clicked <- input[[cellClicked]]
+        private$.reactiveValues$cells_selected <- input[[cellsSelected]]
+        private$.reactiveValues$cell_info <- input[[cellInfo]]
+        private$.reactiveValues$rows_current <- input[[rowsCurrent]]
+        private$.reactiveValues$rows_all <- input[[rowsAll]]
+        private$.reactiveValues$rows_selected <- input[[rowsSelected]]
+        private$.reactiveValues$row_last_clicked <- input[[rowLastClicked]]
+        private$.reactiveValues$columns_selected <- input[[columnsSelected]]
+        private$.reactiveValues$search <- input[[search]]
+        private$.reactiveValues$search_columns <- input[[searchColumns]]
+        private$.reactiveValues$state <- input[[state]]
+      })
+    },
+
     renderTable = function(output) {
       output[[private$id("table")]] <- DT::renderDT(
         expr = private$.data,
@@ -73,6 +128,7 @@ Table <- R6::R6Class(
 
   # Active ----
   active = list(
-    data = function() return(private$.data)
+    data = function() return(private$.data),
+    reactiveValues = function() return(private$.reactiveValues)
   )
 )
