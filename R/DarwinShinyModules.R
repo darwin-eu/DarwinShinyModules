1daf4bab7c6af8688a46c160191750148d0baa0e
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-04-11 15:07:07 +0200

    Added DT

--- R/DarwinShinyModules.R
+++ R/DarwinShinyModules.R
@@ -7,6 +7,7 @@
 #' @import shinydashboard
 #' @import dplyr
 #' @import ggplot2
+#' @import DT
 #' @importFrom plotly event_register event_data plotlyOutput renderPlotly
 ## usethis namespace: end
 NULL
