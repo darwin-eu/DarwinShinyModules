d950b588b93bd11b4243df3ce14edf94d4f55341
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-04-11 15:44:10 +0200

    imported used DT functions from DT

--- R/DarwinShinyModules.R
+++ R/DarwinShinyModules.R
@@ -7,7 +7,7 @@
 #' @import shinydashboard
 #' @import dplyr
 #' @import ggplot2
-#' @import DT
+#' @importFrom DT DTOutput renderDT
 #' @importFrom plotly event_register event_data plotlyOutput renderPlotly
 ## usethis namespace: end
 NULL
