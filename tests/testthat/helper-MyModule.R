30426b98e5b7eddf4634c0cf465ba40be030ef76
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2024-04-11 14:36:06 +0200

    Updated tests

---
+++ tests/testthat/helper-MyModule.R
@@ -0,0 +1,38 @@
+MyModule <- R6::R6Class(
+  classname = "MyModule",
+  inherit = ShinyModule,
+
+  public = list(
+    UI = function() {
+      shiny::tagList(
+        shiny::tableOutput(outputId = shiny::NS(private$.appId, private$id("myTable"))),
+        shiny::uiOutput(outputId = shiny::NS(private$.appId, private$id("moduleData")))
+      )
+    },
+
+    server = function(input, output, session) {
+      output[[private$id("myTable")]] <- shiny::renderTable({
+        head(private$.data)
+      })
+
+      output[[private$id("moduleData")]] <- shiny::renderUI({
+        shiny::HTML(paste(
+          sprintf("App ID: %s", private$.appId),
+          sprintf("Module Name: %s", private$.moduleName),
+          sprintf("Instance ID: %s", private$.instanceId),
+          sprintf("Namespace ID myTable: %s", private$id("myTable")),
+          sprintf("Namespace ID moduleData: %s", private$id("moduleData")),
+          sep = "<br/>"
+        ))
+      })
+    }
+  ),
+
+  private = list(
+    .data = iris
+  ),
+
+  active = list(
+    data = function() return(private$.data)
+  )
+)
