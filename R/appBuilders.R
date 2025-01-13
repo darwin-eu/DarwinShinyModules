e2bf3d2185f207138ac9db8cfa031fb10ba38287
Maarten van Kessel <m.vankessel@mi-erasmusmc.nl>
2025-01-13 17:10:16 +0100

    Updated darwin theme

--- R/appBuilders.R
+++ R/appBuilders.R
@@ -141,6 +141,50 @@ dashboardApp <- function(moduleList, title = NULL) {
   shiny::shinyApp(ui, server)
 }
 
+darwinHeader <- function() {
+  shiny::tagList(
+    shiny::includeCSS(path = system.file(package = "DarwinShinyModules", "www", "style.css")),
+    shiny::tags$body(
+      shiny::tags$div(
+        class = "g-block size-100 darwin-top-line",
+        shiny::tags$div(
+          id = "custom-2346-particle",
+          class = "g-content g-particle"
+        )
+      ),
+      shiny::tags$div(
+        class = "g-grid",
+        shiny::tags$div(
+          class = "header-background-image",
+          style = "padding-top&#x3A;&#x20;40px&#x20;&#x21;important&#x3B;&#x20;padding-left&#x3A;&#x20;25px&#x20;&#x21;important&#x3B;",
+          shiny::tags$div(
+            id = "logo-1935-particle",
+            class = "g-content g-particle",
+            shiny::tags$a(
+              href = "/", class = "g-logo darwin-eu-logo", target = "_self", title = "", rel = "home",
+              shiny::tags$img(
+                src = "https://darwin-eu.org/templates/rt_horizon/custom/images/darwin-eu-logo.png",
+                height = "100px",
+                alt = ""
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+}
+
+darwinFooter <- function() {
+  shiny::tags$footer(
+    shiny::h6(
+      sprintf(
+        "Generated with DarwinShinyModules %s",
+        utils::packageVersion("DarwinShinyModules"))
+    )
+  )
+}
+
 #' darwinApp
 #'
 #' @param moduleList (`list(list())`) A list of named lists, containing modules.
@@ -183,7 +227,8 @@ darwinApp <- function(moduleList, title = "", studyStatus = "ongoing") {
   )
 
   ui <- shiny::tags$body(
-    shiny::tags$img(src = "www/img/darwin-eu-logo.png", height = "100px", align = "top"),
+    shiny::includeCSS(path = system.file(package = "DarwinShinyModules", "www", "theme.css")),
+    darwinHeader(),
     shinydashboard::dashboardPage(
       header = shinydashboard::dashboardHeader(
         title = title,
@@ -196,7 +241,8 @@ darwinApp <- function(moduleList, title = "", studyStatus = "ongoing") {
         ), width = 300
       ),
       body = shinydashboard::dashboardBody(modulesBody(moduleList))
-    )
+    ),
+    darwinFooter()
   )
 
   server = function(input, output, session) {
