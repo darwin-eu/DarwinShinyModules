a7bd5640731f5ff467f85f7ebeae12404a211047
Ger Inberg <ginberg@gmail.com>
2025-05-08 18:04:10 +0200

    update pkgdown, test

--- tests/testthat/test-Incidence.R
+++ tests/testthat/test-Incidence.R
@@ -9,10 +9,23 @@ test_that("Creation: Incidence", {
   incMod <- Incidence$new(data = inc)
 
   expect_identical(class(incMod), c("Incidence", "ShinyModule", "R6"))
+
+  # Fields
+  expect_false(is.null(incMod$namespace))
+  expect_false(is.null(incMod$moduleId))
+  expect_false(is.null(incMod$instanceId))
   expect_identical(class(incMod$pickers), c("list"))
   lapply(names(incMod$pickers), FUN = function(pickerName) {
     expect_identical(class(incMod$pickers[[pickerName]]), c("InputPanel", "ShinyModule", "R6"))
   })
   expect_identical(class(incMod$data), c("tidy_incidence", "tbl_df", "tbl", "data.frame"))
   expect_identical(incMod$moduleName, "Incidence")
+
+  # UI
+  expect_s3_class(incMod$UI(), "shiny.tag.list")
+
+  # Server
+  testServer(app = incMod$server, {
+    expect_true(is.character(session$token))
+  })
 })
