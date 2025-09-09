ad32d7cd79e1dec4a8dafad6f25c25a43efec8d9
Ger Inberg <ginberg@gmail.com>
2025-09-09 10:43:48 +0200

    initial createApp function

---
+++ R/CreateApp.R
@@ -0,0 +1,88 @@
+# Copyright 2024 DARWIN EU®
+#
+# This file is part of DarwinShinyModules
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Function that creates a stand-alone shiny app from a given app structure.
+#'
+#' @param path path where shiny app will be stored
+#' @param appStructure application structure as a list
+#' @param theme theme as a character
+#' @param additionalFiles optional vector of files to be copied
+#' @param createProject if the path should be made into a project
+#' @param openProject if, after creation of the project, the project should be opened
+#'
+#' @export
+#'
+#' @examples
+#' library(DarwinShinyModules)
+#'
+#' base <- Text$new("**base**")
+#' nested_a <- Text$new("**nested A**")
+#' nested_b <- Text$new("**nested B**")
+#' sub_a <- Text$new("**sub A**")
+#' sub_b <- Text$new("**sub B**")
+#' comb_a <- Text$new("**comb A**")
+#' comb_b <- Text$new("**comb B**")
+#' comb_c <- Text$new("**comb C**")
+#'
+#' appStructure <- list(
+#'    base = base,
+#'    nested = list(nested_a, nested_b),
+#'    nested_sub = list(
+#'      sub_a = sub_a,
+#'      sub_b = sub_b
+#'    ),
+#'    nested_combined = list(
+#'      comb_a_b = list(comb_a, comb_b),
+#'      comb_c = comb_c
+#'    )
+#'  )
+#'
+#' if (interactive()) {
+#'   createApp(path = tempdir(),
+#'             appStructure = appStructure,
+#'             theme = 'shinymodules-darwin')
+#' }
+createApp <- function(path, appStructure, theme, additionalFiles = c(), createProject = FALSE, openProject = FALSE) {
+
+  if (!dir.exists(path)) {
+    dir.create(path, recursive = T)
+  }
+
+  themeFun <- switch(
+    theme,
+    "bslib" = "DarwinShinyModules::launchBslibApp(appStructure)",
+    "shinymodules" = "DarwinShinyModules::launchShinydashboardApp(appStructure)",
+    "bslib-darwin" = "DarwinShinyModules::launchDarwinBslibApp(appStructure)",
+    "shinymodules-darwin" = "DarwinShinyModules::launchDarwinDashboardApp(appStructure)"
+  )
+
+  saveRDS(appStructure, file.path(path, "appStructure.rds"))
+
+  writeLines(
+    c("library(DarwinShinyModules)",
+      "appStructure <- readRDS('./appStructure.rds')",
+      themeFun),
+    file.path(path, "app.R")
+  )
+
+  for (file in additionalFiles) (
+    file.copy(from = file, to = file.path(path, basename(file)), recursive = TRUE, overwrite = TRUE)
+  )
+
+  if (createProject) {
+    usethis::create_project(path = path, open = openProject)
+  }
+}
