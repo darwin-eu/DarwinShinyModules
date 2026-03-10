801619fc69bac5eeb452cf490431fbed0071d8a9
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-03-10 12:26:22 +0100

    Updated indication

--- R/DrugUtilisation-Indication.R
+++ R/DrugUtilisation-Indication.R
@@ -14,9 +14,19 @@ Indication <- R6::R6Class(
     initialize = function(result, ...) {
       super$initialize(...)
       private$.result <- result
-      private$.table <- Table$new(data = rawDat, title = NULL, filter = "top", parentNamespace = self$namespace)
 
-      private$.tableMod <- DarwinShinyModules::Flextable$new(fun = )
+      private$.plot <- DarwinShinyModules::PlotPlotly$new(
+        fun = DrugUtilisation::plotIndication,
+        args = list(result = private$.result, style = "darwin"),
+        title = NULL,
+        parentNamespace = self$namespace
+      )
+
+      private$.table <- DarwinShinyModules::Flextable$new(
+        fun = DrugUtilisation::tableIndication,
+        args = list(result = private$.result, type = "flextable", style = "darwin"),
+        parentNamespace = self$namespace
+      )
     }
   ),
 
@@ -25,18 +35,123 @@ Indication <- R6::R6Class(
     ## Fields ----
     .result = NULL,
     .table = NULL,
+    .plot = NULL,
+    .inputPanelTable = NULL,
+    .inputPanelPlot = NULL,
 
     ## Methods ----
     .UI = function() {
       shiny::tagList(
+        shiny::column(
+          width = 2,
+          shinyWidgets::pickerInput(
+            inputId = shiny::NS(self$namespace, "cdm_name"),
+            label = "CDM Name",
+            choices = unique(private$.result$cdm_name),
+            selected = unique(private$.result$cdm_name)[1],
+            multiple = TRUE
+          ),
+          shiny::tabsetPanel(
+            shiny::tabPanel(
+              title = "Table Settings",
+              shinyWidgets::pickerInput(
+                inputId = shiny::NS(self$namespace, "header"),
+                label = "Header",
+                choices = c("cdm_name", "cohort_name", DrugUtilisation::strataColumns(private$.result)),
+                selected = c("cdm_name", "cohort_name", DrugUtilisation::strataColumns(private$.result)),
+                multiple = TRUE
+              ),
+              shinyWidgets::pickerInput(
+                inputId = shiny::NS(self$namespace, "groupColumn"),
+                label = "Group Column",
+                choices = "variable_name",
+                selected = "variable_name",
+                multiple = TRUE
+              ),
+              shinyWidgets::pickerInput(
+                inputId = shiny::NS(self$namespace, "hide"),
+                label = "Columns to hide",
+                choices = c(
+                  "window_name", "mutually_exclusive", "unknown_indication_table",
+                  "censor_date", "cohort_table_name", "index_date",
+                  "indication_cohort_name"
+                ),
+                selected = c(
+                  "window_name", "mutually_exclusive", "unknown_indication_table",
+                  "censor_date", "cohort_table_name", "index_date",
+                  "indication_cohort_name"
+                ),
+                multiple = TRUE
+              )
+            ),
 
+            shiny::tabPanel(
+              title = "Plot Settings",
+              shinyWidgets::pickerInput(
+                inputId = "x_axis",
+                label = "X-Axis",
+                choices = "variable_level",
+                selected = "variable_level"
+              ),
+              shinyWidgets::pickerInput(
+                inputId = "position",
+                label = "Bar position",
+                choices = c("stack", "dodge"),
+                selected = "stack"
+              ),
+              shinyWidgets::pickerInput(
+                inputId = "colour",
+                label = "Colour",
+                choices = private$.getSelectableColumns(),
+                selected = private$.getSelectableColumns()[1]
+              )
+            )
+          )
+        ),
+        shiny::column(
+          width = 10,
+          private$.plot$UI(),
+          private$.table$UI(),
+        )
       )
     },
+
     .server = function(input, output, session) {
-      private$.view_2Mod$server(input, outupt, session)
-      output$view_1 <- gt::render_gt({
-        DrugUtilisation::tableIndication(private$.data, header = "indication")
+      shiny::observeEvent(list(
+        input$cdm_name,
+        input$x,
+        input$position,
+        input$facet,
+        input$colour
+      ), {
+        private$.plot$args$result <- private$.result |>
+          dplyr::filter(.data$cdm_name %in% input$cdm_name)
+
+        private$.plot$args$x <- private$.inputPanelPlot$InputValues$x
+        private$.plot$args$position <- private$.inputPanelPlot$InputValues$position
+        private$.plot$args$facet <- private$.inputPanelPlot$InputValues$facet
+        private$.plot$args$colour <- private$.inputPanelPlot$InputValues$colour
+        private$.plot$server(input, output, session)
       })
+
+      shiny::observeEvent(list(
+        input$cdm_name,
+        input$header,
+        input$groupColumn,
+        input$hide
+      ), {
+        private$.table$args$result <- private$.result |>
+          dplyr::filter(.data$cdm_name %in% input$cdm_name)
+
+        private$.table$args$header <- input$header
+        private$.table$args$groupColumn <- input$groupColumn
+        private$.table$args$hide <- input$hide
+        private$.table$server(input, output, session)
+      })
+    },
+
+    .getSelectableColumns = function() {
+      c(DrugUtilisation::groupColumns(private$.result), DrugUtilisation::strataColumns(private$.result))
     }
   )
 )
