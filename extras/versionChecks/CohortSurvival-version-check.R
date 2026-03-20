8602494b762511f424a52a555141b9cb29121e7c
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-03-20 10:11:49 +0100

    added CohortSurvival version checks

---
+++ extras/versionChecks/CohortSurvival-version-check.R
@@ -0,0 +1,22 @@
+testthat::test_that("CohortCharacteristics", {
+  CDMConnector::requireEunomia()
+  cdm <- CohortSurvival::mockMGUS2cdm()
+
+  result <- CohortSurvival::estimateSingleEventSurvival(
+    cdm = cdm,
+    targetCohortTable = "mgus_diagnosis",
+    outcomeCohortTable = "death_cohort"
+  )
+
+  survMod <- DarwinShinyModules::CohortSurvival$new(result = result)
+
+  testthat::expect_r6_class(survMod, class = "CohortSurvival")
+
+  app <- list(
+    Survival = survMod
+  )
+
+  app <- DarwinShinyModules::launchDarwinDashboardApp(app)
+
+  testthat::expect_identical(class(app), "shiny.appobj")
+})
