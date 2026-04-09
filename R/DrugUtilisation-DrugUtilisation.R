1998229a8b15ab8a20aac787ae1a852fe3d389dd
mvankessel-EMC <m.vankessel@mi-erasmusmc.nl>
2026-04-09 11:56:10 +0200

    Added docs

--- R/DrugUtilisation-DrugUtilisation.R
+++ R/DrugUtilisation-DrugUtilisation.R
@@ -1,3 +1,66 @@
+# Copyright 2026 DARWIN EU®
+#
+# This file is part of DarwinShinyModules
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' @title DrugUtilisation Module Class
+#'
+#' @include ShinyModule.R
+#'
+#' @description
+#' DrugUtilisation module that shows a that supports results from the
+#' `summariseDrugutilisation()` function from the DrugUtilisation package.
+#'
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'  library(DarwinShinyModules)
+#'
+#'  if (
+#'    require(
+#'      "DrugUtilisation",
+#'      character.only = TRUE,
+#'      quietly = TRUE,
+#'      warn.conflicts = FALSE
+#'    )
+#'  ) {
+#'     cdm <- DrugUtilisation::mockDrugUtilisation(numberIndividual = 100, source = "duckdb")
+#'
+#'     cdm <- DrugUtilisation::generateIngredientCohortSet(
+#'       cdm = cdm,
+#'       name = "dus_cohort",
+#'       ingredient = "acetaminophen",
+#'       gapEra = 7
+#'     )
+#'
+#'     result <- cdm$dus_cohort |>
+#'       PatientProfiles::addAge(ageGroup = list(
+#'         `0-17` = c(0, 17),
+#'         `>=18` = c(18, Inf)
+#'       )) |>
+#'       PatientProfiles::addSex() |>
+#'       DrugUtilisation::summariseDrugUtilisation(
+#'         ingredientConceptId = 1125315,
+#'         gapEra = 7,
+#'         strata = list("age_group", "sex")
+#'       )
+#'
+#'     duMod <- DrugUtilisation$new(result = result)
+#'     preview(duMod)
+#'   }
+#' }
 DrugUtilisation <- R6::R6Class(
   classname = "DrugUtilisation",
   inherit = DarwinShinyModules::ShinyModule,
