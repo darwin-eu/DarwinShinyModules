f9609de25132b7593308f166f8ab561d4c500597
mvankessel-EMC <m.l.vankessel@erasmusmc.nl>
2026-06-19 10:22:38 +0200

    Added performance optimisations

--- R/DrugUtilisation-ProportionOfPatientsCovered.R
+++ R/DrugUtilisation-ProportionOfPatientsCovered.R
@@ -1,18 +1,52 @@
+# Copyright 2026 DARWIN EU®
+#
+# This file is part of DarwinShinyModules
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ProportionOfPatientsCovered <- R6::R6Class(
   classname = "ProportionOfPatientsCovered",
   inherit = ShinyModule,
 
-  active = list(),
+  active = list(
+    result = function(result) {
+      if (missing(result)) {
+        return(qs2::qs_deserialize(private$.serialResult))
+      } else {
+        private$.checkResult(result)
+        private$.serialResult <- qs2::qs_serialize(result)
+      }
+    },
+
+    table = function(table) {
+      return(private$.table)
+    },
+
+    plot = function(plot) {
+      return(private$.plot)
+    }
+  ),
 
   public = list(
     initialize = function(result, ...) {
-      checkmate::assert_class(x = result, classes = "summarised_result")
-      checkmate::assert_true(attr(result, "settings")$result_type == "summarise_proportion_of_patients_covered")
+      private$.checkResult(result)
 
-      private$.result <- result
-
-      private$.cdmNames <- getCDMNames(private$.result)
-      private$.cohortNames <- getCohortNames(private$.result)
+      private$.cdmNames <- getCDMNames(result)
+      private$.cohortNames <- getCohortNames(result)
+      private$.tableCols <- availableTableColumns(result)
+      private$.plotCols <- availablePlotColumns(result)
+      private$.strataCols <- DrugUtilisation::strataColumns(result)
+      private$.serialResult <- qs2::qs_serialize(result)
 
       private$.table <- Flextable$new(
         fun = DrugUtilisation::tableProportionOfPatientsCovered,
@@ -23,18 +57,22 @@ ProportionOfPatientsCovered <- R6::R6Class(
       private$.plot <- PlotStatic$new(
         fun = DrugUtilisation::plotProportionOfPatientsCovered,
         args = list(style = "darwin"),
-        parentNamespace = self$namespace
+        parentNamespace = self$namespace,
+        height = "80vh"
       )
     }
   ),
 
   private = list(
-    .result = NULL,
+    .serialResult = NULL,
     .table = NULL,
     .plot = NULL,
 
     .cdmNames = NULL,
     .cohortNames = NULL,
+    .tableCols = NULL,
+    .plotCols = NULL,
+    .strataCols = NULL,
 
     .pickerOptions = list(
       `actions-box` = TRUE,
@@ -92,15 +130,15 @@ ProportionOfPatientsCovered <- R6::R6Class(
           shinyWidgets::pickerInput(
             inputId = shiny::NS(self$namespace, "header"),
             label = "Header",
-            choices = availableTableColumns(result),
-            selected = c("cohort_name", DrugUtilisation::strataColumns(result)),
+            choices = private$.tableCols,
+            selected = c("cohort_name", private$.strataCols),
             multiple = TRUE,
             options = private$.pickerOptions
           ),
           shinyWidgets::pickerInput(
             inputId = shiny::NS(self$namespace, "groupColumn"),
             label = "Group Column",
-            choices = availableTableColumns(result),
+            choices = private$.tableCols,
             selected = "cdm_name",
             multiple = TRUE,
             options = private$.pickerOptions
@@ -127,15 +165,15 @@ ProportionOfPatientsCovered <- R6::R6Class(
           shinyWidgets::pickerInput(
             inputId = shiny::NS(self$namespace, "facet"),
             label = "facet",
-            choices = availablePlotColumns(private$.result),
+            choices = private$.plotCols,
             selected = "cohort_name",
             multiple = TRUE
           ),
           shinyWidgets::pickerInput(
             inputId = shiny::NS(self$namespace, "colour"),
             label = "colour",
-            choices = availablePlotColumns(private$.result),
-            selected = DrugUtilisation::strataColumns(private$.result),
+            choices = private$.plotCols,
+            selected = private$.strataCols,
             multiple = TRUE
           ),
           shinyWidgets::pickerInput(
@@ -161,18 +199,28 @@ ProportionOfPatientsCovered <- R6::R6Class(
 
     .server = function(input, output, session) {
       fetchData <- shiny::reactive({
-        private$.table$args$result <- private$.result |>
-          dplyr::filter(.data$cdm_name %in% input$cdmName) |>
-          omopgenerics::filterGroup(
-            .data$cohort_name %in% input$cohortName
-          )
-      })
+        mirai::mirai({
+          result |>
+            qs2::qs_deserialize() |>
+            dplyr::filter(.data$cdm_name %in% cdmName) |>
+            omopgenerics::filterGroup(
+              .data$cohort_name %in% cohortName
+            )
+        }, result = private$.serialResult, cdmName = input$cdmName, cohortName = input$cohortName)
+      }) |>
+        shiny::bindCache(input$cdmName, input$cohortName)
 
       shiny::observeEvent(input$submitTable, {
         private$.table$args["header"] <- list(input$header)
         private$.table$args["groupColumn"] <- list(input$groupColumn)
-        private$.table$args$result <- fetchData()
-        private$.table$server(input, output, session)
+
+        promises::then(
+          promise = fetchData(),
+          onFulfilled = function(result) {
+            private$.table$args$result <- result
+            private$.table$server(input, output, session)
+          }
+        )
       })
 
       shiny::observeEvent(input$submitPlot, {
@@ -181,16 +229,32 @@ ProportionOfPatientsCovered <- R6::R6Class(
         private$.plot$args["ribbon"] <- list(
           convertLabelToLogical(input$ribbon, trueVal = "On", falseVal = "Off")
         )
-        private$.plot$args$result <- fetchData()
-        private$.plot$server(input, output, session)
+
+        promises::then(
+          promise = fetchData(),
+          onFulfilled = function(result) {
+            private$.plot$args$result <- result
+            private$.plot$server(input, output, session)
+          }
+        )
       })
+    },
+
+    .checkResult = function(result) {
+      collection <- checkmate::makeAssertCollection()
+      checkmate::assert_class(x = result, classes = "summarised_result", add = collection)
+      checkmate::assert_true(attr(result, "settings")$result_type == "summarise_proportion_of_patients_covered", add = collection)
+      checkmate::reportAssertions(collection)
     }
   )
 )
 
-# source("./R/utils.R")
-# source("./R/utils-SummarizedResult.R")
-#
-# mod <- ProportionOfPatientsCovered$new(result)
-#
-# preview(mod)
+source("./R/utils.R")
+source("./R/utils-SummarizedResult.R")
+source("./R/Table-Flextable.R")
+
+mirai::daemons(2)
+
+mod <- ProportionOfPatientsCovered$new(res3)
+
+preview(mod)
